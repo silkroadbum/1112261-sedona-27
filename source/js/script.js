479633c05e5a03badfd70352b3645f1ae032ee39
silkroadbum <54482247+silkroadbum@users.noreply.github.com>
2022-12-15 23:29:36 +0600

    стилизовал и сделал закрытие и открытие мобильного меню

---
+++ source/js/script.js
@@ -0,0 +1,9 @@
+const navMain = document.querySelector('.main-nav');
+const navToggle = document.querySelector('.main-nav__toggle');
+
+navMain.classList.remove('main-nav--nojs');
+
+navToggle.addEventListener('click', () => {
+  navMain.classList.toggle('main-nav--closed');
+  navMain.classList.toggle('main-nav--opened');
+});
